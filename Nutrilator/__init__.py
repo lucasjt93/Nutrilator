c4bd5fa37bc69cfdef8b199e41931b40100cb35d
lucasjt93 <lucasjuliantoledo@hotmail.com>
2021-05-08 14:07:08 +0200

    Refactor of 'app.py' to '__init__.py' && addition of init-db CLI.

--- Nutrilator/__init__.py
+++ Nutrilator/__init__.py
@@ -8,7 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'nutrilator.sqlite'),
     )
 
     if test_config is None:
@@ -23,5 +23,9 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    # initiate db CLI
+    from . import db
+    db.init_app(app)
 
     return app
