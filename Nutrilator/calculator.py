6eb4a7d19d23427d373119a6b31bdd124b4c4bbc
lucasjt93 <lucasjuliantoledo@hotmail.com>
2021-05-23 18:01:52 +0200

    Calculator frontend started

--- Nutrilator/calculator.py
+++ Nutrilator/calculator.py
@@ -9,9 +9,9 @@ from Nutrilator.db import get_db
 bp = Blueprint('calculator', __name__)
 
 
-@bp.route('/calculator')
+@bp.route('/calculator', methods=('GET', 'POST'))
 @login_required
 def calculator():
     username = get_db().execute('SELECT username FROM users WHERE id = ?', (g.user['id'],)).fetchone()
-    message = f'Hi {username[0]} calculate your macros!'
+    message = f'Hi {username[0]}, calculate your macros!'
     return render_template('calculator/calculator.html', message=message)
