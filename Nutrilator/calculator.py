b85b8769f457cf4287cf5e19275d719385b5db2d
lucasjt93 <lucasjuliantoledo@hotmail.com>
2021-05-23 22:01:32 +0200

    Calculator front + back development in progress

--- Nutrilator/calculator.py
+++ Nutrilator/calculator.py
@@ -17,30 +17,52 @@ def calculator():
     message = f'Hi {username[0]}, calculate your macros!'
 
     if request.method == 'POST':
-        # get arguments for calculation
-        age = request.form['age']
-        gender = request.form['gender']
-        weight = request.form['weight']
-        height = request.form['height']
-        activity = request.form['activity']
-        goal = request.form['goal']
-
         # define required fields
         required = {
-            'age':age,
-            'gender':gender,
-            'weight':weight,
-            'height':height,
-            'activity':activity,
-            'goal':goal
+            'age': request.form['age'],
+            'gender': request.form['gender'],
+            'weight': request.form['weight'],
+            'height': request.form['height'],
+            'activity': request.form['activity'],
+            'goal': request.form['goal']
         }
 
-        # handles none in required
+        # handles none in required at serve-side (its also checked at client-side)
         for k, v in required.items():
             if not v:
                 print(k)
                 flash(f'Missing required field {k}', category="error")
                 return render_template('calculator/calculator.html', message=message), 403
 
-        #TODO do the calculation and render calculated.html template
+        # resting energy expenditure (REE)
+        if required['gender'] == 'male':
+            # male
+            REE = 10 * int(required['weight']) + 6.25 * int(required['height']) - 5 * int(required['age']) + 5
+        else:
+            # female
+            REE = 10 * int(required['weight']) + 6.25 * int(required['height']) - 5 * int(required['age']) - 161
+
+        # total daily energy expenditure (TDEE)
+        if required['activity'] == 'Sedentary':
+            TDEE = REE * 1.2
+        elif required['activity'] == 'Light - 3 times per week':
+            TDEE = REE * 1.375
+        elif required['activity'] == 'Mid - 5 times per week':
+            TDEE = REE * 1.55
+        else:
+            TDEE = REE * 1.725
+
+        # determine macros for fat loss
+        weight_loss = TDEE - (TDEE * .20)
+        # determine macros for build muscle
+        weight_gain = TDEE + (TDEE * .20)
+
+        return render_template(
+            'calculator/results.html',
+            TDEE=TDEE,
+            REE=REE,
+            weight_loss=weight_loss,
+            weight_gain=weight_gain
+        )
+
     return render_template('calculator/calculator.html', message=message)
