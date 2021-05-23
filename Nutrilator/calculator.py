693de4e36465ec107569a63a1f4533e7ac639a15
lucasjt93 <lucasjuliantoledo@hotmail.com>
2021-05-23 14:40:12 +0200

    removed print statement

--- Nutrilator/calculator.py
+++ Nutrilator/calculator.py
@@ -13,6 +13,5 @@ bp = Blueprint('calculator', __name__)
 @login_required
 def calculator():
     username = get_db().execute('SELECT username FROM users WHERE id = ?', (g.user['id'],)).fetchone()
-    print(username)
     message = f'Hi {username[0]} calculate your macros!'
     return render_template('calculator/calculator.html', message=message)
