ebd205f3596f9bc1798b9a97248dc1090341ea27
lucasjt93 <lucasjuliantoledo@hotmail.com>
2021-05-25 23:36:05 +0200

    kcal calculator in js done!

--- Nutrilator/static/magic.js
+++ Nutrilator/static/magic.js
@@ -1,22 +1,27 @@
 function Calculate(){
-    if (document.getElementById('male').checked){
-        var ree = (
-        10 * document.getElementById('weight') + 6.25 * document.getElementById('height') - 5 * document.getElementById('age') + 5
-        );
-    } else if (document.getElementById('female').checked){
-        var ree = (
-        10 * document.getElementById('weight') +
-        6.25 * document.getElementById('height') -
-        5 * document.getElementById('age') -
-        161
-        );
-    }
+    let w = document.getElementById('weight').value;
+    let h = document.getElementById('height').value;
+    let a = document.getElementById('age').value;
 
-    if (document.getElementById('activity') == 'Sedentary') {
+    if (document.getElementById('male').checked){
+        var ree = (10 * w + 6.25 * h - 5 * a + 5);
+    } else if (document.getElementById('female').checked){
+        var ree = (10 * w + 6.25 * h - 5 * a + 161);
+    }
+    let s = document.getElementById('activity').value;
+    if (s == 'Sedentary') {
         var tdee = ree * 1.2;
     }
-    var tdee = ree * 2;
+    else if (s == 'Light - 3 times per week') {
+        var tdee = ree * 1.375;
+    }
+    else if (s == 'Mid - 5 times per week') {
+        var tdee = ree * 1.55;
+    }
+    else {
+        var tdee = ree * 1.725;
+    }
 
-    document.getElementById('results').innerHTML = tdee;
+    document.getElementById('results').innerHTML = tdee + ' kcal per day';
 
 }
