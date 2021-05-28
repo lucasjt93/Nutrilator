31572635121840c82cd7548daa0ed6a4b140d02a
lucasjt93 <lucasjuliantoledo@hotmail.com>
2021-05-28 15:18:36 +0200

    Added macros table

--- Nutrilator/schema.sql
+++ Nutrilator/schema.sql
@@ -5,3 +5,12 @@ CREATE TABLE users (
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
 );
+
+CREATE TABLE macros(
+    user_id INTEGER,
+    tdee INTEGER NOT NULL,
+    protein INTEGER NOT NULL,
+    carbo INTEGER NOT NULL,
+    fat INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
