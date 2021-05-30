9fcc8f5be473cb5eb8bc97fc1ea400f985353588
lucasjt93 <lucasjuliantoledo@hotmail.com>
2021-05-30 19:21:45 +0200

    macros displayed in JS

--- Nutrilator/static/magic.js
+++ Nutrilator/static/magic.js
@@ -3,19 +3,23 @@ function Calculate(){
     let h = document.getElementById('height').value;
     let a = document.getElementById('age').value;
     let g = document.getElementById('goal').value;
+    var html = '';
 
     // Check for completion of required fields
     if (!w || !h || !a || !g){
         return document.getElementById('results').innerHTML = 'Make sure all fields are completed!';
     }
-    // Check sex for calculation
+
+    // Check gender for calculation
     if (document.getElementById('male').checked){
         var ree = (10 * w + 6.25 * h - 5 * a + 5);
     } else if (document.getElementById('female').checked){
         var ree = (10 * w + 6.25 * h - 5 * a - 161);
     }
+
     // Retrieve act level
     let s = document.getElementById('activity').value;
+
     // Perform calculation of kcal/day
     if (s == 'Sedentary') {
         var tdee = ree * 1.2;
@@ -30,6 +34,7 @@ function Calculate(){
         var tdee = ree * 1.725;
     }
 
+    // Check goal
     if (g == 'Lose weight') {
         tdee = tdee - (tdee * 0.20);
     }
@@ -37,5 +42,16 @@ function Calculate(){
         tdee = tdee + (tdee * 0.20);
     }
 
-    return document.getElementById('results').innerHTML = tdee + ' kcal per day';
+    // Calculate macros
+    let protein = 0.825 * (w/0.453592);
+    let fat = (tdee * 0.3) / 9;
+    let carbo = (tdee - (protein * 4) - (fat * 9)) / 4
+
+    // Assign to html var
+    html += tdee + ' kcal per day' + '<br/>';
+    html += protein + ' g of protein per day' + '<br/>';
+    html += fat + ' g of fat per day' + '<br/>';
+    html += carbo + ' g of carbos per day' + '<br/>';
+
+    return document.getElementById('results').innerHTML = html;
 }
